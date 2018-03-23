6de15280d93a07d4f10ea9bea54aec12dd4795d4
Prai18 <daichi.sino@gmail.com>
2018-03-24 06:04:28 +0900

    No.354 メルセンヌ素数

--- practice21.c
+++ practice21.c
@@ -1,8 +1,14 @@
+/*2018年1月に歴代最大のメルセンヌ素数（50個目）が発見されました
+メルセンヌ素数とは 2p - 1 の形で表すことのできる素数です
+
+というわけでメルセンヌ素数を2進数で表記したときの各桁の和(つまり1の数)を答えてください
+
+*/
 #include<stdio.h>
 
 int main(int argc, char const *argv[])
 {
-	unsigned int p;			
+	int p;			
 	scanf("%d",&p);
 	printf("%d\n",p);
 
