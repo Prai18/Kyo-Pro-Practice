a70b862432227e7512e8107fea247b4924f90fc0
Prai18 <daichi.sino@gmail.com>
2019-02-28 02:00:49 +0900

    No.793 うし数列2

--- practice29.cpp
+++ practice29.cpp
@@ -1,223 +1,35 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define M 5                //m次多項式のm
-#define N 16               //flagの大きさ
+#include <vector> // vectorを使うためにインクルードする
+#include <iostream>
+using namespace std;
 
-short int Tm_x(short int m);            //m次の2変数可換多項式のxの値
-short int Tm_y(short int m);            //m次の2変数可換多項式のyの値
-void surplus_x(short int huga);         //剰余演算x
-void surplus_y(short int hoge);         //剰余演算y
-void flag_s();                          //flagの初期化
-short int ****malloc_4dim_array(short int a,short int i,short int u,short int e);       //メモリの確保
-void free_4dim_array(short int ****a,short int i,short int u,short int e,short int o);  //メモリの解放
+int a=1;
 
-short int x,y;                 
-short int xx,yy;                        //Tm(x,y)のx,yの値が入る
-short int m;                            //m次多項式
+int main(){
+    vector<int> vc;
+    int N;
+    int num=0,hoge;
+    cin >> N;
 
-short int remnant;                      //余り
+    for(int i=0;i<N;i++){
+        if(i==0){
+            vc.push_back(1);
+        }
+        vc.push_back(3);
+    }
 
-short int prevx,prevy;                  //flag[prevx][prevy][nextx][nexty]
-short int nextx,nexty;
-
-short int count=0;      
-short int cou;
-
-short int l,ll;
-
-short int ****flag;
-
-short int count1,count2,count4,count8,count16,count32;
-short int main(){
-
-    flag=malloc_4dim_array(N,N,N,N);    //メモリの確保
-
-    flag_s();                           //flagの初期化
-
-    for (l = 0; l < N; ++l)
+    for (int i = 0; i < N; ++i)
     {
-        for (ll = 0; ll < N; ++ll)
-        {
-            x=l;
-            y=ll;
-
-            prevx=x;
-            prevy=y;
-
-            while(1){
-
-                for (m = 1; m <= M; ++m){          //1,2,3,・・・,mとTm(x,y)のmを変化させていく
-
-                    xx=Tm_x(m);
-                    yy=Tm_y(m);
-
-                    if(m==M){
-                        surplus_x(xx);
-                        surplus_y(yy);
-                        flag[prevx][prevy][nextx][nexty]+=1;
-                    }
-                }
-                if(flag[prevx][prevy][nextx][nexty]==2){
-                    break;
-                }
-                prevx=nextx;
-                prevy=nexty;
-            }
-
-            x=prevx;
-            y=prevy;
-
-            flag_s();                              //flagの初期化
-
-            while(1){
-
-                for (m = 1; m <= M; ++m){          //1,2,3,・・・,mとTm(x,y)のmを変化させていく
-
-                    xx=Tm_x(m);
-                    yy=Tm_y(m);
-
-                    if(m==M){
-                        surplus_x(xx);
-                        surplus_y(yy);
-                        count++;
-                        flag[prevx][prevy][nextx][nexty]+=1;
-                    }
-                }
-                if(flag[prevx][prevy][nextx][nexty]==2){
-                    printf("%d\n",count-1);
-                    cou=count-1;
-                    if(cou==1){
-                        count1++;
-                    }else if(cou==2){
-                        count2++;
-                    }else if(cou==4){
-                        count4++;
-                    }else if(cou==8){
-                        count8++;
-                    }else if(cou==16){
-                        count16++;
-                    }else if(cou==32){
-                        count32++;
-                    }else{
-                        printf("2冪以外でちゃった\n");
-                    }
-                    count=0;
-                    break;
-                }
-                prevx=nextx;
-                prevy=nexty;
-            }
-
-        }
+        a*=10;
     }
-    printf("周期1の数は→%d\n",count1);
-    printf("周期2の数は→%d\n",count2);
-    printf("周期4の数は→%d\n",count4);
-    printf("周期8の数は→%d\n",count8);
-    printf("周期16の数は→%d\n",count16);
-    printf("周期32の数は→%d\n",count32);
 
-    free_4dim_array(flag, N, N, N, N);               //メモリの解放
-    return 0;
+    for (int i = 0; i < N; ++i){
+       hoge=a;
+       hoge*=vc[i];
+       num+=hoge;
+       a=a/10;
+   }
+   num+=3;
+   cout<<num%1000000007<<endl;
 }
 
 
-
-short int ****malloc_4dim_array(short int n1, short int n2, short int n3,short int n4) {          //メモリの確保
-  short int ****array;
-  array = (short int ****)malloc(n1 * sizeof(short int ***));
-  for (short int i = 0; i < n1; i++) {
-    array[i] = (short int ***)malloc(n2 * sizeof(short int **));
-    for (short int j = 0; j < n2; j++){
-      array[i][j] = (short int**)malloc(n3 * sizeof(short int *));
-      for(short int k=0;k<n3;k++){
-        array[i][j][k] = (short int*)malloc(n4 * sizeof(short int));
-    }
-}
-}
-return array;
-}
-
-
-
-void free_4dim_array(short int ****array, short int n1, short int n2, short int n3,short int n4) {        //メモリの解放
-  for (short int i = 0; i < n1; i++) {
-    for (short int j = 0; j < n2; j++){
-        for (short int k = 0; k < n3; ++k)
-        {
-            free(array[i][j][k]);
-        }
-        free(array[i][j]);
-    }
-    free(array[i]);
-}
-free(array);
-}
-
-
-
-void flag_s(){
-    for (short int i = 0; i < N; ++i)                     
-    {
-        for (short int j = 0; j < N; ++j)
-        {
-            for (short int k = 0; k < N; ++k)
-            {
-                for (short int l = 0; l < N; ++l)
-                {
-                    flag[i][j][k][l]=0;             //flagの初期化
-                }
-            }
-        }
-    }
-}
-
-
-
-void surplus_x(short int huga){
-
-  remnant=huga%N;                                   //Tm(x,y)%2^kのx　
-
-  if(remnant<0){                
-    remnant+=N;
-}
-x=remnant;
-nextx=remnant;
-}
-
-
-
-void surplus_y(short int hoge){
-
-  remnant=hoge%N;                                   //Tm(x,y)%2^kのy　
-
-  if(remnant<0){                
-    remnant+=N;
-}
-y=remnant;
-nexty=remnant;
-}
-
-
-
-short int Tm_x(short int m){                        //2変数可換多項式のxの式(再帰関数)
-    if(m-1==1){
-        return x*x-2*y;
-    }
-    if(m-1==0){
-        return x;
-    }
-    return x*Tm_x(m-1)-y*Tm_x(m-2);
-}
-
-
-
-short int Tm_y(short int m){                        //2変数可換多項式のyの式(再帰関数)
-    if(m-1==1){
-        return y*y;
-    }
-    if(m-1==0){
-        return y;
-    }
-    return y*Tm_y(m-1);
-}
